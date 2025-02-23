70d8762cf26d67f6ce5ef8231264032a17800497
Andone0912 <andonedarius04@gmail.com>
2025-02-23 16:07:23 +0200

    new problem

--- day-1/day-1.js
+++ day-1/day-1.js
@@ -1,3 +1,4 @@
+//makes an array that has all whole numbers up to 10
 /*const arr =[];
 for(let i=1; i < 11; i++){
  arr.push(i)
@@ -5,6 +6,7 @@ for(let i=1; i < 11; i++){
 
 console.log(arr)*/
 
+// finds all odd numbers from 0 to 100
 /*const arr = [];
 for(let i = 0; i < 100; i++){
  if(i % 2 !== 0) {
@@ -14,12 +16,15 @@ for(let i = 0; i < 100; i++){
 
 console.log(arr)*/
 
+
+//the multiplication table for 7
 /*let result;
 for(let i=0; i<11; i++) {
 result = 7* i
 console.log(7 * ${i} = ${result})
 }*/
 
+//multiplication table up to 10
 /*let result;
 for(let x=0; x<11;x++) {
  for(let i=0; i<11;i++) {
@@ -33,13 +38,14 @@ for(let i=0; i<11; i++){
 }
 console.log(result)*/
 
-
+// multiplies all numbers up to 10
 /*let result=1
-for(let i=1; i<15; i++){
+for(let i=1; i<11; i++){
  result= result * i
 }
 console.log(result)*/
 
+//the sum of all odd numbers from 10 to 30
 /*let result=0;
 let arr=[]
 for(let i=0; i<31; i++){
@@ -50,11 +56,12 @@ for(let i=0; i<31; i++){
 }
 console.log(result)*/
 
+// converts celsius to farenheit
 /*const celsius = 20
 let farenheitFormula = (celsius * 9/5) + 32;
 console.log(farenheitFormula)*/
 
-
+// Calculates the sum of all nums in an array
 /*let result=0
 arr=[3, 5, 11, 17, 4, 9]
 for(let i=0; i < arr.length; i++){
@@ -62,49 +69,50 @@ for(let i=0; i < arr.length; i++){
 }
  console.log(result)*/
 
-
+// Finds the average in an array of nums
 /*let sum = 0
 const arr = [5, 42, 20, -10, -20, 15]
 for(let i=0; i < arr.length; i++){
  sum += arr[i]
 }
-
 sum = sum / arr.length
-
 console.log(sum)*/
 
+//filters out positive numbers from an array
 /*arr=[-2, 3, 5, -8, 10, -40]
-
 function sortNumbers() {
 arr2=[]
 for(let i=0; i < arr.length; i++){
  if(arr[i] < 0) {
    arr2.push(arr[i])
 }}
- return arr2;
-}
-
+return arr2;}
 sortNumbers()
 console.log(arr2)*/
 
+//Finds the biggest nr from an array
 /*let num=0
-
 function findMax() {
 arr =[2, 35, 12, 10, 100]
-
 for(let i=0; i < arr.length; i++){
  if(arr[i] > num){
-  num = arr[i]
-}}
-return num;
-}
+  num = arr[i]}}
+return num;}
 findMax()
 console.log(num)*/
 
-arr=[0, 1]
+//Fibonacci sequence
+/*arr=[0, 1]
 for(let i = 0; i < 11; i++){
   if(i > 1) {
     arr[i]= arr[i-1] + arr[i-2]
   }
 }
-console.log(arr)
+console.log(arr)*/
+
+arr1 = []
+for(let i = 1; arr1.length === 10; i++) {
+  const n =  Math.round(Math.random() * 10)
+  for(let e = 0; e < arr1.length; e++) {} 
+}
+console.log(arr1)
